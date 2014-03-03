55e53be6bed16421f53e4d03df5ec1e5eb62d395
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-03-03 13:35:41 -0300

    Fix views

--- serialization/urls.py
+++ serialization/urls.py
@@ -4,7 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('snippets.views',
-    url(r'^snippets/$', 'snippet_list')
-    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail')
+
+    url(r'^snippets/$', 'snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+
+
+
     url(r'^admin/', include(admin.site.urls)),
    )
