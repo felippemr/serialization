1efd5a6d884780dd03f6ed0c08f86ea7bd54524c
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-03-04 17:19:09 -0300

    Adds suport to multiple formats...json,yaml,xml...

--- serialization/urls.py
+++ serialization/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,3 +10,5 @@ urlpatterns = patterns('snippets.views',
     url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)
